8277d6db7714919c2dfbc2682ee6e181ac7e48c4
linyuanjin <linyuanjin@bytedance.com>
2021-01-04 19:39:14 +0800

    SimpleLogger::Get

--- hikv.cc
+++ hikv.cc
@@ -35,7 +35,7 @@ uint64_t SimpleLogger::Add(const std::string_view& k,
   uint64_t offset = curr_offset_.fetch_add(request_len);
   if (offset + request_len > len_) {
     PRINT_FILE_LINE();
-    PrintErrorThenExit("out of space");
+    PrintErrorThenExit("out of space. offset: " + std::to_string(offset));
   }
   assert(offset < (1ULL << 48));
 
@@ -50,12 +50,30 @@ uint64_t SimpleLogger::Add(const std::string_view& k,
   memcpy(cursor, k.data(), k.size());
   cursor += k.size();
   memcpy(cursor, v.data(), v.size());
+  pmem_persist(cursor, request_len);
   return offset;
 }
 
 bool SimpleLogger::Get(uint64_t offset, const std::string_view& k,
                        std::string* v) const {
-  return {};
+  uint32_t k_len;
+  uint32_t v_len;
+
+  char* cursor = reinterpret_cast<char*>(addr_) + offset;
+  memcpy(&k_len, cursor, sizeof(k_len));
+  cursor += sizeof(k_len);
+  memcpy(&v_len, cursor, sizeof(v_len));
+  cursor += sizeof(v_len);
+
+  std::string_view store_k(cursor, k_len);
+  if (store_k == k) {
+    if (v != nullptr) {
+      cursor += k_len;
+      v->assign(cursor, v_len);
+    }
+    return true;
+  }
+  return false;
 }
 
 void* OpenPMemFileThenInit(const std::string& path, size_t len) {
